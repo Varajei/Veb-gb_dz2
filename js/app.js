b2689807c7b2e7ac85c030c0cde739145fb8af3a
Varajei <alibovie@mail.ru>
2023-06-29 21:53:52 +0300

    2

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//Ввод данных
+
+let age = prompt("Сколько вам лет?");
+
+//Логика 
+age = parseInt(age)
+if (age > 18 && age <= 99) {
+    console.log("Вы совершеннолетний!")
+} 
+
+
+//Вывод данных
+console.log(age)
